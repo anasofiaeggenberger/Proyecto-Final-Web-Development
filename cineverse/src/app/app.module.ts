b3b0cc627babe89ce4ce51b869687dca8323f435
anasofiaeggenberger <156016977+anasofiaeggenberger@users.noreply.github.com>
2025-11-04 19:06:38 -0600

    Estructura base optimizada: componente layout global (navbar + footer) y routing inicial

---
+++ cineverse/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LayoutComponent } from './core/layout/layout.component';
+import { IndexComponent } from './pages/index/index.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LayoutComponent,
+    IndexComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
