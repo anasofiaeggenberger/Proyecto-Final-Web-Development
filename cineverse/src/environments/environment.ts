1835580807948247415021e55856b05b7151b150
anasofiaeggenberger <156016977+anasofiaeggenberger@users.noreply.github.com>
2025-11-21 01:31:36 -0600

    Fix API URL for production

--- cineverse/src/environments/environment.ts
+++ cineverse/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
-    tmdbApiKey: '1af7819c872d3f74aa4331af561b02d2',
-    apiUrl: 'http://localhost:3000/api' // 👈 Backend local
-  };  
+  production: false,
+  tmdbApiKey: '1af7819c872d3f74aa4331af561b02d2',
+  apiUrl: 'https://cineverse-api-production.up.railway.app/api'
+};
