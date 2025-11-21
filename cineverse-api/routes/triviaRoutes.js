f2f5f21647d6cb3938ba63f323c6bddf30defb65
anasofiaeggenberger <156016977+anasofiaeggenberger@users.noreply.github.com>
2025-11-20 23:56:56 -0600

    backend dirigido a otro repo

--- cineverse-api/routes/triviaRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-// Ruta temporal
-router.get('/', (req, res) => {
-  res.send('Trivia route funcionando 🚀');
-});
-
-export default router;
